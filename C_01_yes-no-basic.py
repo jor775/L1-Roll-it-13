aec130430ef2bc655fe6f64a4277974e1de53d1b
jor775 <jordan.wyman@papamoacollege.school.nz>
2025-03-27 11:40:49 +1300

    Update C_01_yes-no-basic.py

--- C_01_yes-no-basic.py
+++ C_01_yes-no-basic.py
@@ -1,9 +1,16 @@
-want_instructions = input("Do you wan to see the instructions? ").lower()
+while True:
 
-# Check if the user says yes / no
-if want_instructions == "yes" or want_instructions == "y":
-    print("you said yes")
-elif want_instructions == "no":
-    print ("you said no")
-else:
-    print("please enter yes / no")
+    want_instructions = input("Do you wan to see the instructions? ").lower()
+
+     # check the user says yes / no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("you said yes")
+        break
+    elif want_instructions == "no":
+        print ("you said no")
+        break
+    else:
+        print("please enter yes / no")
+        continue
+
+print("we done")
